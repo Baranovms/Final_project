ec6567530fbed624879d51dc411158c7cc337c2b
BaranovMS <baranov.mihail@mail.ru>
2024-03-12 15:14:55 +0300

    update files

--- recipesapp/urls.py
+++ recipesapp/urls.py
@@ -4,16 +4,16 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='Главная'),
-    path('register/', views.register, name='Регистрация'),
-    path('login/', views.user_login, name='Профиль'),
-    path('logout/', auth_views.LogoutView.as_view(), name='Выйти'),
-    path('edit_profile/', views.edit_profile, name='Редактировать профиль'),
-    path('search/', views.search_recipes, name='Поиск'),
-    path('recipe/<int:recipe_id>', views.get_recipe, name='Рецепт №'),
-    path('recipe/<slug:recipe_name>', views.get_recipe_by_name, name='Рецепты от автора'),
-    path('add_recipe/', views.add_recipe, name='Добавить рецепт'),
-    path('edit_recipe/<int:recipe_id>/', views.edit_recipe, name='Редактировать рецепт'),
-    path('recipes/', views.get_recipes, name='Получить рецепт'),
-    path('recipes/<str:user>/', views.get_recipes, name='Рецепты от пользователя'),
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('search/', views.search_recipes, name='search_recipes'),
+    path('recipe/<int:recipe_id>', views.get_recipe, name='recipe'),
+    path('recipe/<slug:recipe_name>', views.get_recipe_by_name, name='recipe_by_name'),
+    path('add_recipe/', views.add_recipe, name='add_recipe'),
+    path('edit_recipe/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
+    path('recipes/', views.get_recipes, name='get_recipes'),
+    path('recipes/<str:user>/', views.get_recipes, name='get_recipes_by_user'),
 ]
